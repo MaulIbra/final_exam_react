46d0a1293f633fb7d9d25866f72be46c410476fd
maulana <maulana@mymindstores.com>
2020-08-02 09:50:01 +0700

    [MODIFY] maps component

--- src/component/MapsComponent.js
+++ src/component/MapsComponent.js
@@ -1,24 +1,34 @@
 import React from 'react';
 import Chart from "react-google-charts";
-import regionData from '../regionData.json'
+import _ from 'lodash';
+import regionData from '../regionData.json';
 const MapsComponent = ({keys}) => {
 
-    console.log(regionData[keys])
+    let ISO_array = []
+    ISO_array.push(['Provinces', 'Population % '],[regionData[keys],null])
+    _.forEach(regionData, function(value) {
+        if (value.id !== regionData[keys]){
+            let dataMap = []
+            dataMap.push(value.id,value.population)
+            ISO_array.push(dataMap)
+        }
+    });
     return (
         <Chart
             className="map"
             chartType="GeoChart"
-            data={[
-                ["abv", "population%"],
-                [`${regionData[keys] ? regionData[keys].id: "ID-AC"}`, `${regionData[keys] ? regionData[keys].population: 2}`],
-            ]}
+            data={
+                ISO_array
+            }
             mapsApiKey="AIzaSyAnwjnAqQckxUUOxsRq_Ajzzya8VV1u9IU"
             options={{
-                region: "ID",
-                resolution: "provinces",
-                colors: ['blue']
+                datalessRegionColor: '#123456',
+                colorAxis: {colors: ['#00853f', 'black', '#e31b23']},
+                resolution:'provinces',
+                defaultColor: 'green',
+                region: 'ID',
             }}
-            rootProps={{ 'data-testid': '1' }}
+            rootProps={{ 'data-testid': '2' }}
         />
     );
 };
